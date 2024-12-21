506f4c643997c7f9b630e289de646b82c4438641
Евгений Кучма <desferto87@gmail.com>
2024-12-21 22:44:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homeWorkControlEntering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitCommand = "exit";
+            string inputUser = " ";
+
+            while (inputUser != exitCommand)
+            {
+                Console.Write("Enter the correct command to exit the programm: ");
+                inputUser = Console.ReadLine();
+
+                if (inputUser == exitCommand)
+                {
+                    Console.WriteLine("Correct command, good bye!");
+                }              
+            }
+        }
+    }
+}
